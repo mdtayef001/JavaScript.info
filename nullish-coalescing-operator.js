d5d6030893c260a7d8d28cd0757430c44369cfc0
Tayef <tayefhossain200@gmail.com>
2023-12-28 19:05:36 +0600

    nullish star

---
+++ nullish-coalescing-operator.js
@@ -0,0 +1,6 @@
+// Nullish coalescing operator '??'
+// ?? returns the first argument if it’s not null/undefined. Otherwise, the second one.
+
+let a = null;
+let result =
+  a !== null && a !== undefined ? console.log(a) : console.log(`a is = ${a}`);
