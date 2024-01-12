da652d9601290f98bd6dca916a4ee1be4f0dece5
tayef <tayefhossain200@gmail.com>
2024-01-12 15:39:28 +0600

    test

--- nullish-coalescing-operator.js
+++ nullish-coalescing-operator.js
@@ -4,3 +4,14 @@
 let a = null;
 let result =
   a !== null && a !== undefined ? console.log(a) : console.log(`a is = ${a}`);
+
+const parent = (n) => {
+  const num = n;
+  return function (num) {
+    return num;
+  };
+};
+
+console.log(parent(10));
+
+console.log("hi");
