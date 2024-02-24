5beb73b7d6b8f7e701a1aed77a69ae546ff7498e
tayef <tayefhossain200@gmail.com>
2024-02-24 17:03:41 +0600

    the_switc_statement

---
+++ the_switch_statement.js
@@ -0,0 +1,8 @@
+const num = 1000000;
+console.log(num.toLocaleString("en-IN"));
+console.log(Math.min(2, 3, 42, 41, 23, 3, 45));
+console.log(Math.max(2, 3, 42, 41, 23, 3, 45));
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
